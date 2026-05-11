d82ef4a83aaab7ce93573fba4ca66590ee8d4bbb
ozcantaga <41130371+ozcantaga@users.noreply.github.com>
2026-05-11 18:55:25 +0300

    Update useMenu.ts

--- app/composables/useMenu.ts
+++ app/composables/useMenu.ts
@@ -124,17 +124,17 @@ export const useMenu = () => {
           image: "/images/wine/wine.webp",
           position: 'center',
           items: [
-            { id: 'w1', name: "alcohol.red", cl: "18 cl / 75 cl", price:780, bottlePrice: 1560, icon: "wine_bar",glass:"min_2 Glass" },
-            { id: 'w3', name: "alcohol.white", cl: "18 cl / 75 cl", price:780, bottlePrice: 1560, icon: "wine_bar",glass:"min_2 Glass" },
-            { id: 'w5', name: "alcohol.ros", cl: "18 cl / 75 cl", price:780, bottlePrice: 1560, icon: "wine_bar",glass:"min_2 Glass" }
+            { id: 'w1', name: "alcohol.red", cl: "18 cl / 75 cl", price:780, bottlePrice: 1560, icon: "wine_bar" },
+            { id: 'w3', name: "alcohol.white", cl: "18 cl / 75 cl", price:780, bottlePrice: 1560, icon: "wine_bar"},
+            { id: 'w5', name: "alcohol.ros", cl: "18 cl / 75 cl", price:780, bottlePrice: 1560, icon: "wine_bar" }
           ]
         },
         "spirits": {
           image: "/images/visky/visky.webp",
           position: 'center',
           items: [
-            { id: 'sp1', name: "alcohol.whisky_gin", cl: "4 cl / 70 cl", price: 700, bottlePrice: 6500, icon: "glass_cup" ,glass:"Glass"},
-            { id: 'sp2', name: "alcohol.raki_70", cl: "4 cl", price: 350, bottlePrice: 3000, icon: "liquor" ,glass:"Glass" },
+            { id: 'sp1', name: "alcohol.whisky_gin", cl: "4 cl / 70 cl", price: 700, bottlePrice: 6500, icon: "glass_cup"},
+            { id: 'sp2', name: "alcohol.raki_70", cl: "4 cl", price: 350, bottlePrice: 3000, icon: "liquor"  },
             
           ]
         }
